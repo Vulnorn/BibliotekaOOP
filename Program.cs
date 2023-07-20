e9c5cb3e9050cd89d3cbf074f373b9dd17ec82b0
Vulnorn <mixadrbk31@gmail.com>
2023-07-20 23:43:24 +0300

    Поиск по году издания

--- Program.cs
+++ Program.cs
@@ -13,13 +13,14 @@ namespace BibliotekaOOP
         {
             Shelf shelf = new Shelf();
             shelf.AddBook();
-
+            shelf.ShowAllBooks();
+           Console.ReadKey();
         }
     }
 
     class Book
     {
-        public Book(string name, string author, string yearRelease )
+        public Book(string name, string author, int yearRelease)
         {
             Name = name;
             Author = author;
@@ -27,7 +28,7 @@ namespace BibliotekaOOP
         }
         public string Name { get; private set; }
         public string Author { get; private set; }
-        public string YearRelease { get; private set; }
+        public int YearRelease { get; private set; }
 
         public void ShowInfo()
         {
@@ -45,50 +46,66 @@ namespace BibliotekaOOP
         {
             Console.Clear();
 
-            string name = InputUser( "Введите название книги");
-            string author = InputUser( "Введите автора книги");
-            string yearRelease = InputUser( "Введите Год издания книги");
+            string name = InputUserString("Введите название книги");
+            string author = InputUserString("Введите автора книги");
 
-            _books.Add(new Book(name, author, yearRelease));
+            if (GetInputUserYear(out int yearPublication)==true)
+                _books.Add(new Book(name, author, yearPublication));
+            else
+                Console.WriteLine("Книга не добывлена!");
+
         }
 
-       public void DeleteBook()
+        public void DeleteBook()
         {
-            if (TryGetBook(out Book book))
+            Console.WriteLine("Введите номер книги для удаления");
+
+            if (GetBookAccordingId(out Book book)==true)
             {
-                _books.Remove( book);
+                _books.Remove(book);
                 Console.WriteLine("Книга с таким номером удалена");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Нет такой книги");
                 Console.ReadKey();
             }
         }
 
-        public void Show ()
+        public void ShowAllBooks()
         {
             Console.Clear();
-            for (int i=0;i<_books.Count();i++)
+            for (int i = 0; i < _books.Count(); i++)
             {
-                Console.Write($"№ { i + 1}) ");
+                Console.Write($"№ {i + 1}) ");
                 _books[i].ShowInfo();
             }
         }
-        private string InputUser( string massage)
+
+        public void ShowBooksAccordingYearPublication()
         {
 
-            string userInput = null;
+        }
 
-            while (VerifyInput(userInput))
+        private string InputUserString(string massage)
+        {
+            string userInput=null;
+
+            do
             {
                 Console.WriteLine(massage);
                 userInput = Console.ReadLine();
 
-                if (userInput == null)
+                if (GetInputString(userInput) == true)
                     Console.WriteLine("Поле не может быть пустым");
             }
+            while (GetInputString(userInput));
 
             return userInput;
         }
 
-        private bool VerifyInput(string input)
+        private bool GetInputString(string input)
         {
             if (input == null)
                 return true;
@@ -96,20 +113,54 @@ namespace BibliotekaOOP
                 return false;
         }
 
-        private bool TryGetBook(out Book book)
+        private bool GetInputUserYear(out int yearPublication)
         {
-            int id;
-            book = null;
+            Console.WriteLine("Введите Год издания книги");
+            string userInput = Console.ReadLine();
 
-            Console.WriteLine("Введите номер книги для удаления");
-            string userInput = Console.ReadLine().Trim();
+            if ( GetInputValue(userInput,out yearPublication)==false)
+                return false;
 
-            if (int.TryParse(userInput, out id) == false)
+            if (GetNumberRange(yearPublication))
+            {
+                Console.WriteLine("Год издания книги не может быть нулем или отрицательным числом");
+                Console.ReadKey();
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool GetInputValue(string input, out int number)
+        {
+            if (int.TryParse(input, out number) == false)
             {
                 Console.WriteLine("Не корректный ввод.");
                 Console.ReadKey();
                 return false;
             }
+
+            return true;
+        }
+
+        private bool GetNumberRange(int number)
+        {
+            int minimunYear = 1;
+
+            if (number < minimunYear)
+                return true;
+
+            return false;
+        }
+
+        private bool GetBookAccordingId(out Book book)
+        {
+            book = null;
+
+            string userInput = Console.ReadLine();
+
+            if (GetInputValue(userInput,  out int id) == false)
+                return false;
 
             id--;
 
@@ -122,11 +173,31 @@ namespace BibliotekaOOP
                 }
             }
 
-            Console.WriteLine("Нет такой книги");
-            Console.ReadKey();
             return false;
         }
 
+        private bool GetAllBooksAccordingYearPublication()
+        {
+            List<Book> books = new List<Book>(); 
+
+            string userInput = Console.ReadLine();
+
+            if (GetInputValue(userInput, out int year) == false)
+                return false;      
+
+            for (int i = 0; i < _books.Count; i++)
+            {
+                if (year  == _books[i].YearRelease)
+                {
+                    books.Add(_books[i]);
+                }
+            }
+
+            if (books.Count > 0)
+                return true;
+
+            return false;
+        }
 
     }
 }
